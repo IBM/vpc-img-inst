4a02dcab75f0ba3c878cf82a69d23fa2d1fa99c1
Cohen-J-Omer <cohen.j.omer@gmail.com>
2022-11-13 19:51:25 +0200

    add constants file for shared defaults

--- constants.py
+++ constants.py
@@ -1,1 +1,4 @@
-DEFAULTS = {'base_image_name':'ibm-ubuntu-20-04-4-minimal-amd64-2', 'region':'us-south', 'installation_type':"Ubuntu", "image_create_retries":3}
+DEFAULTS = {'base_image_name':'ibm-ubuntu-20-04-4-minimal-amd64-2',  
+            'region':'us-south',
+             'installation_type':"Ubuntu", 
+             "image_create_retries":3}
