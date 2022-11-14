8ed9a8916eeff3f90a348fb4d986cbe0dd91ce76
Cohen-J-Omer <cohen.j.omer@gmail.com>
2022-11-14 19:39:18 +0200

    added dynamically created constants

--- constants.py
+++ constants.py
@@ -1,4 +1,9 @@
+
+import os
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))  # absolute path to project's root folder.
 DEFAULTS = {'base_image_name':'ibm-ubuntu-20-04-4-minimal-amd64-2',  
             'region':'us-south',
              'installation_type':"Ubuntu", 
-             "image_create_retries":3}
+             "image_create_retries":3, 
+             "input_file":f'{DIR_PATH}{os.sep}defaults.yaml',
+             "output_folder":f"{DIR_PATH}{os.sep}logs{os.sep}"}
