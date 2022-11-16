520388db84b17732164fb513f6e7da50f699b082
Cohen-J-Omer <cohen.j.omer@gmail.com>
2022-11-16 17:52:06 +0200

    remove docker inst. for RHEL

--- src/ibm_vpc_img_inst/installation_scripts/docker/install_docker_rhel.sh
+++
@@ -1,6 +0,0 @@
-yum install -y yum-utils
-yum-config-manager \
-    --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo
-yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin  
-systemctl enable docker.service
