4c47a33264bb2b8827f161fb659067f3365673f4
Cohen-J-Omer <cohen.j.omer@gmail.com>
2022-12-26 16:13:24 +0200

    discard redundant append to PATH

--- src/vpc_img_inst/config_modules/delete_resources.py
+++ src/vpc_img_inst/config_modules/delete_resources.py
@@ -20,7 +20,6 @@ import sys
 from typing import Any, Dict
 
 from vpc_img_inst.constants import DEFAULTS
-sys.path.append('../generate_gpu_image')
 from vpc_img_inst.config_builder import ConfigBuilder
 from vpc_img_inst.utils import color_msg, Color, logger
 from ibm_cloud_sdk_core import ApiException
